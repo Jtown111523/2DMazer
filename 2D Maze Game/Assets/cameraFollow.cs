e172417d9edbfff563574b1f9904d25139c4a47e
Jack <jjhaek@cox.net>
2018-06-06 13:23:24 -0700

    Camera Follow

---
+++ 2D Maze Game/Assets/cameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+  public Transform playerPos;
+
+    Vector3 offset = new Vector3(0, 0, -10);
+
+
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 cameraPos = playerPos.position + offset;
+
+        this.transform.position = cameraPos;
+		
+	}
+}
